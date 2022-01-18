39239b87f457819cda47f113f6b57151e2f643a9
Natthaphong Phengsa <natthaphong.phengsa.syne20lin@tucsweden.se>
2022-01-18 11:24:01 +0100

    Delete Seed comment

--- src/EduManagementLab.Web/Program.cs
+++ src/EduManagementLab.Web/Program.cs
@@ -17,12 +17,6 @@ builder.Services.AddTransient<CourseService>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-//using (var scope = app.Services.CreateScope())
-//{
-//    var serviceProvider = scope.ServiceProvider;
-//    var dbContext = serviceProvider.GetRequiredService<DataContext>();
-//    DevTestData.Seed(dbContext);
-//}
 
     // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
