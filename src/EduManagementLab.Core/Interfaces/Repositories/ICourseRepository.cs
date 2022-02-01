5b799f0cdee9f825cdcac2b436a91f9e4f3ccfb0
Vernnie Chong <vernnie.chong.syne20lin@tucsweden.se>
2022-02-01 10:10:04 +0100

    Added function in course service to fetch courses data in user page

--- src/EduManagementLab.Core/Interfaces/Repositories/ICourseRepository.cs
+++ src/EduManagementLab.Core/Interfaces/Repositories/ICourseRepository.cs
@@ -4,6 +4,7 @@ namespace EduManagementLab.Core.Interfaces.Repositories
 {
     public interface ICourseRepository : IGenericRepository<Course>
     {
+        IEnumerable<Course.Membership>? GetUserCourses(Guid userId);
         Course? GetCourse(Guid courseId, bool includeMembershipUsers);
     }
 }
