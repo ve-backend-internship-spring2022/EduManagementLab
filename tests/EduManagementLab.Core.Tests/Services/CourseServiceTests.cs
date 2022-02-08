539ad0eb2052c3dc4f4a16b689ef6fdc08df5dd8
Edwin Gustafsson <EdwinGustafsson@hotmail.com>
2022-02-08 14:42:52 +0100

    Updated CourseServiceTests

--- tests/EduManagementLab.Core.Tests/Services/CourseServiceTests.cs
+++ tests/EduManagementLab.Core.Tests/Services/CourseServiceTests.cs
@@ -11,36 +11,46 @@ namespace EduManagementLab.Core.Tests.Services
 {
     public class CourseServiceTests
     {
-        private readonly DbContextOptions<DataContext> _contextOptions;
-
-
-        public CourseServiceTests()
-        {
-            _contextOptions = new DbContextOptionsBuilder<DataContext>()
+        private readonly DbContextOptions<DataContext> _contextOptions = new DbContextOptionsBuilder<DataContext>()
                 .UseInMemoryDatabase("CourseServiceTest")
                 .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
                 .Options;
 
-            using var context = new DataContext(_contextOptions);
+        private readonly DataContext _dataContext;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly CourseService _courseService;
+        DataContext CreateContext() => new DataContext(_contextOptions);
+        UnitOfWork CreateUnitOfWork() => new UnitOfWork(_dataContext);
+        CourseService CreateCourseService() => new CourseService(_unitOfWork);
+        public CourseServiceTests()
+        {
 
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+            _dataContext = CreateContext();
 
-            context.AddRange(
-                new Course { Id = Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), Code = "AAA", Name = "CourseNameOne", Description = "CourseDescriptionOne", StartDate = DateTime.MinValue, EndDate = DateTime.MaxValue },
-                new Course { Id = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"), Code = "BBB", Name = "CourseNameTwo", Description = "CourseDescriptionTwo", StartDate = DateTime.MinValue, EndDate = DateTime.MaxValue });
+            _dataContext.Database.EnsureDeleted();
+            _dataContext.Database.EnsureCreated();
 
-            context.SaveChanges();
+            Course course1 = new Course { Id = Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), Code = "AAA", Name = "CourseNameOne", Description = "CourseDescriptionOne", StartDate = DateTime.MinValue, EndDate = DateTime.MaxValue };
+            Course course2 = new Course { Id = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"), Code = "BBB", Name = "CourseNameTwo", Description = "CourseDescriptionTwo", StartDate = DateTime.MinValue, EndDate = DateTime.MaxValue };
+
+            User user1 = new User { Id = Guid.Parse("8E7A4A48-9FFE-4E66-8AF5-65B7860CFEC0"), Displayname = "DisplayNameOne", Email = "EmailOne@Test.com", FirstName = "FirstNameOne", LastName = "LastNameOne" };
+
+            Course.Membership membership1 = new Course.Membership { Id = Guid.Parse("9C1BA350-62EC-4A90-BD85-647CD15159ED"), Course = course2, CourseId = course2.Id, User = user1, UserId = user1.Id, EnrolledDate = DateTime.MinValue };
+
+            _dataContext.AddRange(
+                course1, course2,
+                user1);
+
+            _dataContext.SaveChanges();
+
+            _unitOfWork = CreateUnitOfWork();
+            _courseService = CreateCourseService();
         }
 
         [Fact]
-        public void GetCourse()
+        public void GetCourse_ReturnsCorrectCourse()
         {
-            using var context = CreateContext();
-
-            var unitOfWorkGet = new UnitOfWork(context);
-            var courseServiceGet = new CourseService(unitOfWorkGet);
-            var fetchedCourse = courseServiceGet.GetCourse(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"));
+            var fetchedCourse = _courseService.GetCourse(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"));
 
             Assert.NotNull(fetchedCourse);
             Assert.Equal("AAA", fetchedCourse.Code);
@@ -51,13 +61,9 @@ namespace EduManagementLab.Core.Tests.Services
         }
 
         [Fact]
-        public void GetAllCourses()
+        public void GetAllCourses_ReturnsCorrectCourses()
         {
-            using var context = CreateContext();
-
-            var unitOfWorkGet = new UnitOfWork(context);
-            var courseServiceGet = new CourseService(unitOfWorkGet);
-            var fetchedCourses = courseServiceGet.GetCourses();
+            var fetchedCourses = _courseService.GetCourses();
 
             Assert.Collection(
                 fetchedCourses,
@@ -66,42 +72,83 @@ namespace EduManagementLab.Core.Tests.Services
         }
 
         [Fact]
-        public void AddCourse()
+        public void AddCourse_ReturnsCorrectCourse()
         {
-            using var context = CreateContext();
+            var createdCourse = _courseService.CreateCourse("CCC", "CourseNameThree", "CourseDescriptionThree", DateTime.Today, DateTime.Today);
 
-            var unitOfWorkCreate = new UnitOfWork(context);
-            var courseServiceCreate = new CourseService(unitOfWorkCreate);
-            var createdCourse = courseServiceCreate.CreateCourse("CCC", "CourseNameThree", "CourseDescriptionThree", DateTime.Today, DateTime.Today);
-
-            var course = context.Courses.Single(b => b.Code == "CCC");
+            var course = _dataContext.Courses.Single(b => b.Code == "CCC");
             Assert.Equal("CCC", course.Code);
         }
 
         [Fact]
-        public void UpdateCourse()
+        public void UpdateCourseInfo_ReturnsCorrectCourse()
         {
-            using var context = CreateContext();
+            _courseService.UpdateCourseInfo(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), "AAB", "ChangedCourseName", "ChangedCourseDescription");
 
-
-            var unitOfWorkUpdate = new UnitOfWork(context);
-            var courseServiceUpdate = new CourseService(unitOfWorkUpdate);
-
-            courseServiceUpdate.UpdateCourseInfo(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), "AAB", "ChangedCourseName", "ChangedCourseDescription");
-            courseServiceUpdate.UpdateCoursePeriod(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), DateTime.Today, DateTime.Today);
-
-
-            var course = context.Courses.Single(b => b.Code == "AAB");
+            var course = _dataContext.Courses.Single(b => b.Id == Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"));
 
             Assert.NotNull(course);
             Assert.Equal("AAB", course.Code);
             Assert.Equal("ChangedCourseName", course.Name);
             Assert.Equal("ChangedCourseDescription", course.Description);
+        }
+
+        [Fact]
+        public void UpdateCourseEmail_ReturnsCorrectCourse()
+        {
+            _courseService.UpdateCoursePeriod(Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"), DateTime.Today, DateTime.Today);
+
+            var course = _dataContext.Courses.Single(b => b.Id == Guid.Parse("4E228873-0468-4BE6-A14B-48DE5E7CFFFB"));
+
+            Assert.NotNull(course);
             Assert.Equal(DateTime.Today, course.StartDate);
             Assert.Equal(DateTime.Today, course.EndDate);
         }
 
-        DataContext CreateContext() => new DataContext(_contextOptions);
+        [Fact]
+        public void DeleteCourse_DeletesCorrectCourse()
+        {
+            _courseService.DeleteCourse(Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"));
+
+            var fetchedCourses = _courseService.GetCourses();
+
+            Assert.Equal(1 ,fetchedCourses.Count());
+        }
+
+        [Fact]
+        public void CreateCourseMembership_ReturnsCorrectCourse()
+        {
+            var testCourseId = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D");
+            var testUserId = Guid.Parse("8E7A4A48-9FFE-4E66-8AF5-65B7860CFEC0");
+            var testEnrollmentDate = DateTime.MinValue;
+
+            var course = _courseService.CreateCourseMembership(testCourseId, testUserId, testEnrollmentDate);
+
+            Assert.Equal(Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"), course.Id);
+        }
+
+        [Fact]
+        public void RemoveCourseMembership_ReturnsCorrectCourse()
+        {
+            var testCourseId = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D");
+            var testUserId = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D");
+           
+            var course = _courseService.RemoveCourseMembership(testCourseId, testUserId);
+
+            Assert.Equal(Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"), course.Id);
+        }
+
+        [Fact]
+        public void UpdateMembershipEnrolledDate_ReturnsCorrectCourse()
+        {
+            var testCourseId = Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D");
+            var testUserId = Guid.Parse("8E7A4A48-9FFE-4E66-8AF5-65B7860CFEC0");
+            var testEnrollmentDate = DateTime.MinValue;
+
+            var course = _courseService.CreateCourseMembership(testCourseId, testUserId, testEnrollmentDate);
+
+            Assert.Equal(Guid.Parse("4A0E4335-08E0-45C0-8A97-9791CE81E73D"), course.Id);
+        }
 
     }
 }
