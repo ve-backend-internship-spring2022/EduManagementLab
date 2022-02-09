1af498f3572b7c362bf133bd865e254050bb2206
Edwin Gustafsson <EdwinGustafsson@hotmail.com>
2022-02-09 12:23:27 +0100

    Changed return object and updated tests

---
+++ src/EduManagementLab.Core/Exceptions/MembershipAlreadyExistException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduManagementLab.Core.Exceptions
+{
+    public class MembershipAlreadyExistException : Exception
+    {
+        public MembershipAlreadyExistException() : base($"This user is already a member of this course")
+        {
+        }
+    }
+}
